b359c12544faa8e2fb09b3bbfbedda9e4c2cb179
Krešimir Delija <net4metal@gmail.com>
2019-07-31 13:53:48 +0200

    Updates

---
+++ precache-manifest.5c17342e90bb605a5d63737e0944ca80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c77156d1107b32e536c32ea4aa7dfc",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "14e42df80fe9ab9226ab",
+    "url": "/Monsters-Rolodex/static/css/main.bb216b29.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monsters-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "14e42df80fe9ab9226ab",
+    "url": "/Monsters-Rolodex/static/js/main.b1540178.chunk.js"
+  },
+  {
+    "revision": "3c42c109e430c626051d",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.2f1dcf62.js"
+  }
+]);
